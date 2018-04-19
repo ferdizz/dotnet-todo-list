13cd36c743351049a997e840e95c4c78fa9ac4fc
ferdizz <ferdinandforgaard@gmail.com>
2018-04-20 00:07:27 +0200

    Simple User-model

---
+++ server/Models/User.cs
@@ -0,0 +1,13 @@
+
+namespace dotnet_todo_list.Models
+{
+    public class User
+    {
+        public long ID { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
