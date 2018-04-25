2f6f9610577324caeb4d5ca3364e52161fda58e5
ferdizz <ferdinandforgaard@gmail.com>
2018-04-25 18:41:25 +0200

    Added the Login and UserList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Login from './components/Login';
+import UserList from './components/UserList';
+import './styles.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
+      <div className="container">
+        <div>
+          {
+            this.props.user.email
+              ? ''
+              : <Login />
+          }
+        </div>
+        <UserList />
       </div>
     );
   }
+
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, null)(App);
