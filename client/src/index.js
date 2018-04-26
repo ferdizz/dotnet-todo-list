9ffbc0924e9f27f9176891f39783c09a246e1988
ferdizz <ferdinandforgaard@gmail.com>
2018-04-26 02:12:38 +0200

    Added Redux devtools

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,8 @@ import { createStore } from 'redux';
 import combinedReducer from './reducers';
 
 const store = createStore(
-    combinedReducer
+    combinedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 ReactDOM.render(
